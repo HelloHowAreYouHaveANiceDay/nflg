f46b83ce05ac0083e8931b39199f2db519a93075
hellohowareyouhaveaniceday <liboli.lilibo@gmail.com>
2019-07-17 21:27:13 -0400

    scorestrip

--- src/nfl/ScoreStrip.ts
+++ src/nfl/ScoreStrip.ts
@@ -1,4 +1,46 @@
 import axios from 'axios';
+import * as convert from 'xml-js'
+
+interface xml_ss extends convert.ElementCompact{
+    _declaration: {
+        _attributes: {
+            encoding: string,
+            version: string
+        }
+    },
+    ss: {
+        gms: {
+            _attributes: {
+                gd: number,
+                t: string,
+                w: string,
+                y: string
+            },
+            g: xml_game[]
+        }
+    }
+}
+
+interface xml_game {
+    _attributes: {
+        d: string;
+        eid: number;
+        ga: string;
+        gsis: number;
+        gt: "REG" | "POST" | "PRE";
+        h: string;
+        hnn: string;
+        hs: number;
+        k: string;
+        p: string;
+        q: string;
+        rz: string;
+        t: string;
+        v: string;
+        vnn: string;
+        vs: number;
+    }
+}
 
 
 export async function get_ss(season: number, seasonType: string, week: number) {
@@ -10,14 +52,20 @@ export async function get_ss(season: number, seasonType: string, week: number) {
             responseType: 'text'
         });
 
-        const parser = new DOMParser();
-
         // console.log(response);
-        console.log(response.status)
+        // console.log(response.data)
 
-        return parser.parseFromString(response.data, 'text/xml');
+        const data = convert.xml2js(response.data, { compact: true }) as xml_ss
 
-    } catch(err) {
+        // console.log(data)
+
+        const games = data.ss.gms.g.map((e: xml_game) => {
+            return e._attributes
+        });
+
+        return games;
+
+    } catch (err) {
         console.log(err)
         // throw err
     }
